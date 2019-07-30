42ed735b7310f0c31dbab463bcccdde873caa705
nevo <nevo@securenative.com>
2019-07-30 18:27:10 +0300

    added proper dotnet example

---
+++ Securenative-dotnet/Response.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string apiKey = "apiKey";
+        var sn = new SecureNative.SDK.SecureNative(apiKey, new SecureNative.SDK.Models.SecureNativeOptions());
+        bool isOk = SecureNative.SDK.VerifyWebhook.IsRequestFromSecureNative(HttpContext.Current, apiKey);
+     }
+}
---
+++ Securenative-dotnet/Verify.aspx.cs
@@ -0,0 +1,26 @@
+﻿using SecureNative.SDK;
+using SecureNative.SDK.Enums;
+using System;
+using System.Web;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string apiKey = "apiKey";
+        var sn = new SecureNative.SDK.SecureNative(apiKey, new SecureNative.SDK.Models.SecureNativeOptions()
+        {
+            ApiUrl = "https://api.securenative.com/collector/api/v1",
+            Interval = 1000,
+            MaxEvents = 1000,
+            Timeout = 1500,
+            AutoSend = true
+        });
+       
+
+        var verified = sn.Verify(new SecureNative.SDK.Models.EventOptions("sn.user.login")
+        {
+            IP = "162.247.74.201"
+        });
+    }
+}
