e7a54848220ed65810c38ce73481ecb4dfbb386f
Inbal Tako <inbal@securenative.com>
2019-07-30 16:28:29 +0300

    Fix syntax

--- Securenative-ruby/app/controllers/events_controller.rb
+++ Securenative-ruby/app/controllers/events_controller.rb
@@ -3,14 +3,14 @@ require 'securenative'
 class EventsController < ApplicationController
   def verify
     begin
-      sn = SecureNative("YOUR API KEY", SecureNativeOptions.new)
+      sn = SecureNative("YOUR API KEY", options: SecureNativeOptions.new)
       sn.verify(Event.new(
           event_type = EventTypes::LOG_IN,
-          user = User.new("1", "Jon Snow", "jon@snow.com"),
-          ip = "1.2.3.4",
-          remote_ip = "5.6.7.8",
-          user_agent = "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
-          sn_cookie = "cookie"))
+          user: User.new("1", "Jon Snow", "jon@snow.com"),
+          ip: "1.2.3.4",
+          remote_ip: "5.6.7.8",
+          user_agent: "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
+          sn_cookie: "cookie"))
 
       @message = "verified"
     rescue SecureNativeSDKException => e
@@ -20,14 +20,14 @@ class EventsController < ApplicationController
 
   def track
     begin
-      sn = SecureNative("YOUR API KEY", SecureNativeOptions.new)
+      sn = SecureNative("YOUR API KEY", options: SecureNativeOptions.new)
       sn.track(Event.new(
           event_type = EventTypes::SIGN_UP,
-          user = User.new("1", "Jon Snow", "jon@snow.com"),
-          ip = "1.2.3.4",
-          remote_ip = "5.6.7.8",
-          user_agent = "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
-          sn_cookie = "cookie"))
+          user: User.new("1", "Jon Snow", "jon@snow.com"),
+          ip: "1.2.3.4",
+          remote_ip: "5.6.7.8",
+          user_agent: "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
+          sn_cookie: "cookie"))
 
       @message = "tracked"
     rescue SecureNativeSDKException => e
