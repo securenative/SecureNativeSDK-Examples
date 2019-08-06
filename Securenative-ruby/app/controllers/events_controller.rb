0dc39fe58af949598ac9d4476ecb7e17bfb5de00
Inbal Tako <inbal@securenative.com>
2019-08-06 14:24:13 +0300

    Use a singleton pattern

--- Securenative-ruby/app/controllers/events_controller.rb
+++ Securenative-ruby/app/controllers/events_controller.rb
@@ -1,37 +1,36 @@
 require 'securenative'
+require 'securenative/event_type'
+require 'securenative/event_options'
 
 class EventsController < ApplicationController
-  def verify
-    begin
-      sn = SecureNative("YOUR API KEY", options: SecureNativeOptions.new)
-      sn.verify(Event.new(
-          event_type = EventTypes::LOG_IN,
-          user: User.new("1", "Jon Snow", "jon@snow.com"),
-          ip: "1.2.3.4",
-          remote_ip: "5.6.7.8",
-          user_agent: "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
-          sn_cookie: "cookie"))
+  begin
+    SecureNative.init("YOUR API KEY", options: SecureNativeOptions.new)
+  rescue SecureNativeSDKException => e
+    # Do some error handling
+    print(e)
+  end
 
-      @message = "verified"
-    rescue SecureNativeSDKException => e
-      @message = e.backtrace
-    end
+  def verify
+    res = SecureNative.verify(Event.new(
+        event_type = EventTypes::LOG_IN,
+        user: User.new("1", "Jon Snow", "jon@snow.com"),
+        ip: "1.2.3.4",
+        remote_ip: "5.6.7.8",
+        user_agent: "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
+        sn_cookie: "cookie"))
+
+    @message = res
   end
 
   def track
-    begin
-      sn = SecureNative("YOUR API KEY", options: SecureNativeOptions.new)
-      sn.track(Event.new(
-          event_type = EventTypes::SIGN_UP,
-          user: User.new("1", "Jon Snow", "jon@snow.com"),
-          ip: "1.2.3.4",
-          remote_ip: "5.6.7.8",
-          user_agent: "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
-          sn_cookie: "cookie"))
+    SecureNative.track(Event.new(
+        event_type = EventTypes::SIGN_UP,
+        user: User.new("1", "Jon Snow", "jon@snow.com"),
+        ip: "1.2.3.4",
+        remote_ip: "5.6.7.8",
+        user_agent: "Mozilla/5.0 (Linux; U; Android 4.4.2; zh-cn; GT-I9500 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.0 QQ-URL-Manager Mobile Safari/537.36",
+        sn_cookie: "cookie"))
 
-      @message = "tracked"
-    rescue SecureNativeSDKException => e
-      @message = e.backtrace
-    end
+    @message = "tracked"
   end
 end
