16590fcc85db1fd08c53cf67f2a3b5dfed04f9aa
Inbal Tako <inbal@securenative.com>
2020-10-07 10:51:36 +0300

    Add dotnet skeleton website

---
+++ dotnet-sdk-asp.net/dotnet-sdk-asp.net/SecureNativeSDK.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SecureNative.SDK.Config;
+
+namespace dotnet_sdk_asp.net
+{
+    public class SecureNativeSDK
+    {
+        public static SecureNativeOptions options = ConfigurationManager.ConfigBuilder()
+                    .WithApiKey("246F87DD33AF3C9D2E2A55B115580AF37F849244").Build();
+
+        public static SecureNative.SDK.SecureNative securenative = SecureNative.SDK.SecureNative.Init(options);
+    }
+}
