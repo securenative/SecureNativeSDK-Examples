c2fd97f19230c282f268edc144326439d72a1355
Inbal Tako <inbal@securenative.com>
2020-10-20 16:39:11 +0300

    Align dotnet example

--- dotnet-sdk-asp.net/dotnet-sdk-asp.net/Controllers/EventsController.cs
+++ dotnet-sdk-asp.net/dotnet-sdk-asp.net/Controllers/EventsController.cs
@@ -21,13 +21,13 @@ namespace dotnet_sdk_asp.net.Controllers
         // GET: /events/track
         public string Track()
         {
-            // var context = SecureNative.SDK.SecureNative.ContextBuilder()
-            //     .WithIp("127.0.0.1")
-            //     .WithClientToken("9944fb8bb855bcf4d4db50e1a6ed268dc921a8e7a30da4163c4592944b2d098d27124eb7bf8eb04e409ad6242c7419961b5b1174464b5cf2e516346183fbbdb9818566cecad41bbce974369b9ea7b911741285d1952a593ef3ac4267a44b15600f92fa3c92dd9e2d95e8417763121feeeeeaf803fd38d7576dbf7c6364a68c50475cf4006c44081a49373159ed53925f6772db5637c0d81eb63285077dcfa2de36406bad133f1f8e6ed32a5e17efbb6fc9d5eaea954b6828df0c2ea5a7630d6c")
-            //     .WithHeaders(new Dictionary<string, string> { { "user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36" } })
-            //     .Build();
+            var context = SecureNative.SDK.SecureNative.ContextBuilder()
+                 .WithIp("127.0.0.1")
+                 .WithClientToken(Environment.GetEnvironmentVariable("SECURENATIVE_CLIENT_TOKEN"))
+                 .WithHeaders(new Dictionary<string, string> { { "user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36" } })
+                 .Build();
 
-            var context = SecureNativeContextBuilder.FromHttpRequest(Request).Build();
+            /*var context = SecureNativeContextBuilder.FromHttpRequest(Request).Build();*/
 
             var eventOptions = EventOptionsBuilder.Builder(EventTypes.LOG_IN)
                 .WithUserId("2020")
@@ -47,7 +47,7 @@ namespace dotnet_sdk_asp.net.Controllers
         {
             /*var context = SecureNative.SDK.SecureNative.ContextBuilder()
                 .WithIp("127.0.0.1")
-                .WithClientToken("9944fb8bb855bcf4d4db50e1a6ed268dc921a8e7a30da4163c4592944b2d098d27124eb7bf8eb04e409ad6242c7419961b5b1174464b5cf2e516346183fbbdb9818566cecad41bbce974369b9ea7b911741285d1952a593ef3ac4267a44b15600f92fa3c92dd9e2d95e8417763121feeeeeaf803fd38d7576dbf7c6364a68c50475cf4006c44081a49373159ed53925f6772db5637c0d81eb63285077dcfa2de36406bad133f1f8e6ed32a5e17efbb6fc9d5eaea954b6828df0c2ea5a7630d6c")
+                .WithClientToken(Environment.GetEnvironmentVariable("SECURENATIVE_CLIENT_TOKEN"))
                 .WithHeaders(new Dictionary<string, string> { { "user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36" } })
                 .Build();*/
             
