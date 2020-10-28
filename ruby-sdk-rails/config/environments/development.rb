23510d4a5a6dbe8458277c1bd605feb068105a9c
Inbal Tako <inbal@securenative.com>
2020-10-28 13:56:14 +0200

    Update ruby to version 0.1.38

--- ruby-sdk-rails/config/environments/development.rb
+++ ruby-sdk-rails/config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.hosts << "b39de89e4cc7.ngrok.io"
+  config.hosts << "518f5df1b692.ngrok.io"
 
   # Show full error reports.
   config.consider_all_requests_local = true
